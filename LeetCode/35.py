804a500334e25a2a29ae34d0e1f34af7637e550d
Md Piyal Ahmmed <yasinarafath21@gmail.com>
2025-08-20 18:22:31 +0600

    Leetcode 35 added and updated LowerBound Code

--- LeetCode/35.py
+++ LeetCode/35.py
@@ -1,7 +1,9 @@
-# lower bound 
+# --> leetcode 35 . 
+# --> Search Insert Position
 
 
-nums = [ 1,1,1,2,3,3,5,6,7,7,7,9,12,12,14,19,19,19,20]
+
+nums = [1,3,5,6]
 
 
 
@@ -22,3 +24,6 @@ def lowerBound(nums,target):
             low = mid +1
             
     return lb
+
+k = lowerBound(nums,5)
+print(k)
