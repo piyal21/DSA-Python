4026cee369b66bed2b81bd6262e2817a47acf4d2
Md Piyal Ahmmed <yasinarafath21@gmail.com>
2025-09-06 00:59:02 +0600

    updated solutions

--- LeetCode/88.py
+++ LeetCode/88.py
@@ -3,7 +3,7 @@ class Solution:
         
         last = m + n - 1
         
-        
+        # Merge in reverse order
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[last] = nums1[m - 1]
