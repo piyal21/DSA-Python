d4a2df6d816f5bebe3c8f23012d8a40d6c5a63f0
Md Piyal Ahmmed <yasinarafath21@gmail.com>
2025-09-08 00:21:12 +0600

    81 solutoin added

---
+++ LeetCode/81.py
@@ -0,0 +1,38 @@
+# Here the list holds duplicate values. 
+
+
+nums =[ 7,7,7,7,7,7,7,1,2,3,4,5,7,7]
+
+
+def FindValue(nums,target):
+    n = len(nums)
+    low ,high = 0, n-1
+    
+    while low <= high:
+        mid = (low+high)//2
+        
+        if nums[mid] == target:
+            return True
+        
+        if nums[low] == nums[mid] == nums[high]:
+            low = low +1 
+            high = high -1 
+            continue
+        
+        if nums[mid] <= nums[high]:
+            if nums[mid] <= target <= nums[high]:
+                low = mid+1
+                
+            else:
+                high = mid - 1
+                
+        else:
+            if nums[low] <= target <= nums[mid]:
+                high = mid - 1 
+            else:
+                low = mid + 1 
+    return False
+
+
+k = FindValue(nums,3)
+print(k)
