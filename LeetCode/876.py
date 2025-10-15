04e6114f1809df53d9055075cf838a9288f96ae8
Md Piyal Ahmmed <yasinarafath21@gmail.com>
2025-10-15 09:37:50 +0600

    updated code

---
+++ LeetCode/876.py
@@ -0,0 +1,15 @@
+#Find the middle of a linked list 
+# -> Tortoise and hare approach 
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next 
+            fast=fast.next.next
+        return slow
+    
+
+
