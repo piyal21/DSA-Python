f7919d14f6f2828eace4b86e0e09d60bf470a4fd
Md Piyal Ahmmed <yasinarafath21@gmail.com>
2025-10-16 16:47:00 +0600

    updated code

---
+++ LeetCode/142.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# Problem : Find the node where the cycle begins in a linked list.
+# If there is no cycle, return null.
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head 
+        fast = head 
+        while fast is not None and fast.next is not None:
+            slow = slow.next 
+            fast = fast.next.next 
+            if slow == fast :
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return None
